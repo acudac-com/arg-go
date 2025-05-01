5c7a316a397f2e2b120ca8db4db2510ffe7f273b
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-05-01 05:57:07 +0200

    feat: FallbackIfNil

--- arg.go
+++ arg.go
@@ -76,3 +76,11 @@ func (a *Arg[T]) FallbackIf(fallback T, condition bool) *Arg[T] {
 	}
 	return a
 }
+
+// Returns the value if the provided value is nil, otherwise the fallback
+func FallbackIfNil[T any](value *T, fallback *T) *T {
+	if value == nil {
+		return fallback
+	}
+	return value
+}
